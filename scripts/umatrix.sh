ad9e5d11bd4315f1b3ae71a35473cf2e02e7650e
thewisenerd <thewisenerd@protonmail.com>
2016-12-24 18:05:16 +0530

    scripts: umatrix: do not patch in user dir

--- scripts/umatrix.sh
+++ scripts/umatrix.sh
@@ -5,6 +5,7 @@ local __path="${csm_extensions_dir}/${__dir}"
 
 local __name="uMatrix.chromium"
 local __build="${__path}/dist/build/uMatrix.chromium"
+local __dest="${CSM_HOME}/${CSM_EXTS_DIR}/${__name}"
 
 if [ "$(test_folder_read "${__path}")" == "1" ]; then
 	logv_warn "${__path} not readable, skip loading ${__name}\n"
@@ -37,20 +38,19 @@ fi
 
 tools/make-chromium.sh > /dev/null
 
+register_extension "${__name}" "${__build}"
 
 # copy current uMatrix settings
 uMsqlPath="${HOME}/.mozilla/firefox/user.default/extension-data/umatrix.sqlite"
 local __assets="${1}/umatrix"
 if [[ "$(test_file_read "${uMsqlPath}" )" == "0" ]]; then
-	patch -t -p0 < "${__assets}/01-uMatrix-load-userMatrix.patch" > /dev/null 2>&1
+	patch -t -p0 -d "${__dest}" < "${__assets}/01-uMatrix-load-userMatrix.patch" > /dev/null 2>&1
 	if [ $? -ne 0 ]; then
 		logv_warn "patching uMatrix failed.\n"
 	fi
 
-	sqlite3 ${uMsqlPath} "select value from settings where name = \"userMatrix\";" | sed s/^\"//g | sed s/\"\$//g | sed s/"\\\\n"/"\n"/g > ${__build}/assets/umatrix/userMatrix.txt
+	sqlite3 ${uMsqlPath} "select value from settings where name = \"userMatrix\";" | sed s/^\"//g | sed s/\"\$//g | sed s/"\\\\n"/"\n"/g > "${__dest}/assets/umatrix/userMatrix.txt"
 fi
-
-register_extension "${__name}" "${__build}"
 
 # restore cwd
 cd ${CSM_PWD}
